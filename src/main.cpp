749061d6722ac2113c607e99be8f2bac07f91155
ttdow <travis.t.dow@gmail.com>
2025-12-26 15:58:39 -0700

    Added GLFW and Vulkan SDK.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    glfwInit();
 
-    return 0;
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Application", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported.\n";
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
 }
